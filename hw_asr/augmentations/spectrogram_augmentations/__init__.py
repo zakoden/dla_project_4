d8eca89b000427b090e6c9ccfdb4e918154c088b
Zakoden <zakhar.kondratiev@mail.ru>
2022-10-15 23:39:25 +0300

    fix init

--- hw_asr/augmentations/spectrogram_augmentations/__init__.py
+++ hw_asr/augmentations/spectrogram_augmentations/__init__.py
@@ -1,1 +1,6 @@
+from hw_asr.augmentations.spectrogram_augmentations.TimeMasking import TimeMasking
+from hw_asr.augmentations.spectrogram_augmentations.FrequencyMasking import FrequencyMasking
 
+__all__ = [
+    "TimeMasking", "FrequencyMasking"
+]
