f25e366f4787f082d8e9f26a3b4f0750fb5a4f8c
Zakoden <zakhar.kondratiev@mail.ru>
2022-10-15 23:35:45 +0300

    add 2 augmentations

---
+++ hw_asr/augmentations/spectrogram_augmentations/TimeMasking.py
@@ -0,0 +1,12 @@
+from torch import Tensor
+import torchaudio
+
+from hw_asr.augmentations.base import AugmentationBase
+
+
+class TimeMasking(AugmentationBase):
+    def __init__(self, *args, **kwargs):
+        self._aug = torchaudio.transforms.TimeMasking(*args, **kwargs)
+
+    def __call__(self, data: Tensor):
+        return self._aug(data)
