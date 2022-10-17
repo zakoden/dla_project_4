c5a900bee0db029c5d46f9f1969ce4bc932d8513
zakoden <59803738+zakoden@users.noreply.github.com>
2022-10-17 23:04:35 +0300

    Update calc_metrics.py

--- calc_metrics.py
+++ calc_metrics.py
@@ -72,7 +72,7 @@ def main(config, out_file):
                 pred_text_argmax = text_encoder.ctc_decode(argmax.cpu().numpy())
                 pred_text_beam_search = text_encoder.ctc_beam_search(batch["probs"][i],
                                                                      batch["log_probs_length"][i],
-                                                                     beam_size=100)[0].text
+                                                                     beam_size=3)[0].text
 
                 check_metrics["argmax_CER"].append(calc_cer(target_text, pred_text_argmax))
                 check_metrics["beam_search_CER"].append(calc_cer(target_text, pred_text_beam_search))
